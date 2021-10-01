45dba45e03ed661427c9ebf44964499bffda258f
Alexander Dyrikov <dyrikov.a@gmail.com>
2021-10-01 15:53:51 +0300

    import only needed classes

--- src/service/FileService.java
+++ src/service/FileService.java
@@ -1,7 +1,18 @@
 package service;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
 
 public class FileService {
 
--- src/service/JavaProgramService.java
+++ src/service/JavaProgramService.java
@@ -1,6 +1,11 @@
 package service;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
